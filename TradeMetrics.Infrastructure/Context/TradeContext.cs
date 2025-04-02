e04f6b548e0725e09135ab985ee2dc71c837be0c
Wartelon Malthus <Wartelon.malthus@gmail.com>
2025-04-02 16:48:36 -0300

    Configurando context da applicacao

--- TradeMetrics.Infrastructure/Context/TradeContext.cs
+++ TradeMetrics.Infrastructure/Context/TradeContext.cs
@@ -1,6 +1,15 @@
-﻿namespace TradeMetrics.Infrastructure.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using TradeMetrics.Domain;
 
-public class TradeContext
+namespace TradeMetrics.Infrastructure.Context;
+
+public class TradeContext : DbContext
 {
-    
+	public TradeContext(DbContextOptions<TradeContext> options)
+	: base(options)
+	{
+	}
+
+	public DbSet<Operation> Operacao { get; set; }
+
 }
